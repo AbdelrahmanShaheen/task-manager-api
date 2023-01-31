769004d8a94ac273ff5394f8781e61f7f16bd649
AbdelrahmanShaheen <77184432+AbdelrahmanShaheen@users.noreply.github.com>
2023-01-31 02:58:25 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 const app = require("./app");
 const port = process.env.PORT;
 app.use((req, res, next) => {
+  console.log("LOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOL");
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
