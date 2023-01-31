f846a8558046ddf4411804da07208f49b605885f
AbdelrahmanShaheen <77184432+AbdelrahmanShaheen@users.noreply.github.com>
2023-01-31 02:42:07 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 // STARTING POINT FOR THE WHOLE APP.
 const app = require("./app");
 const port = process.env.PORT;
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 app.listen(port, () => {
   console.log("Server is Running on port " + port);
 });
