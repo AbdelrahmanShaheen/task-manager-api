5078fb607899acd43fa3334cfd30d62e7cb048a2
AbdelrahmanShaheen <77184432+AbdelrahmanShaheen@users.noreply.github.com>
2023-01-31 07:46:30 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,7 +4,12 @@ const taskRouter = require("./routers/task");
 
 const express = require("express");
 const app = express();
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
