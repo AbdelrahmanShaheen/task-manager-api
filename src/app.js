f6de75cc4d619f3acca2758f95ec48f0d7abe5da
AbdelrahmanShaheen <77184432+AbdelrahmanShaheen@users.noreply.github.com>
2023-01-31 02:33:51 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,5 +8,9 @@ const app = express();
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 
 module.exports = app;
