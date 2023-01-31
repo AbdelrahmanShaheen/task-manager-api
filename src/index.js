c253c1fa9ad4942875e4bee26e61b45ae2da8534
AbdelrahmanShaheen <77184432+AbdelrahmanShaheen@users.noreply.github.com>
2023-01-31 02:52:46 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ const app = require("./app");
 const port = process.env.PORT;
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
   next();
 });
 app.listen(port, () => {
