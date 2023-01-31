aefb31e93879739d1321e24642546ff7ef265572
AbdelrahmanShaheen <77184432+AbdelrahmanShaheen@users.noreply.github.com>
2023-01-31 07:46:04 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
 // STARTING POINT FOR THE WHOLE APP.
 const app = require("./app");
 const port = process.env.PORT;
-app.use((req, res, next) => {
-  console.log("LOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOL");
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-  res.header("Access-Control-Allow-Headers", "Authorization");
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-  next();
-});
+
 app.listen(port, () => {
   console.log("Server is Running on port " + port);
 });
